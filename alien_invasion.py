d63fb9070c3c588147eb910826822d23e4ebad09
miirzii <shamirssj3@gmail.com>
2025-04-06 21:28:15 -0400

    Created a ship class and set up to draw to screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -19,6 +20,8 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
+        self.ship = Ship(self)
+
     def run_game(self):
         # Game loop
         while self.running:
@@ -29,7 +32,7 @@ class AlienInvasion:
                     sys.exit()
                     
             self.screen.blit(self.bg, (0,0))
-
+            self.ship.draw()
             pygame.display.flip()
             self.clock.tick(self.settings.FPS)
 
--- settings.py
+++ settings.py
@@ -9,3 +9,6 @@ class Settings:
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
         
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
+        self.ship_w = 40
+        self.ship_h = 60
