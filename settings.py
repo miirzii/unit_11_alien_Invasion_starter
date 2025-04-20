c8709b6ef30f20b684bc9d595cea50677d08c21e
miirzii <shamirssj3@gmail.com>
2025-04-20 15:50:01 -0400

    p2- have aliens drop when hitting edges

--- settings.py
+++ settings.py
@@ -24,5 +24,6 @@ class Settings:
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
         self.alien_w = 40
         self.alien_h = 40
-        self.fleet_speed = 2
+        self.fleet_speed = 5
         self.fleet_direction = 1
+        self.fleet_drop_speed = 40
